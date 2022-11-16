11f3b2a25994fe6b11fbc7334e1eabfdc47a36b7
ryutaroiseki <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-11-16 15:32:04 +0900

    fix: remove not use in test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,15 +8,6 @@ void main() {
     'Counter increments smoke test',
     (tester) async {
       await tester.pumpWidget(const MyApp());
-
-      expect(find.text('0'), findsOneWidget);
-      expect(find.text('1'), findsNothing);
-
-      await tester.tap(find.byIcon(Icons.add));
-      await tester.pump();
-
-      expect(find.text('0'), findsNothing);
-      expect(find.text('1'), findsOneWidget);
     },
   );
 }
