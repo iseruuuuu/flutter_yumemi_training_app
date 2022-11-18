354f369018495cca7ebb2120c00cd7498fc8a849
ryutaroiseki <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-11-18 10:21:51 +0900

    fix: remove test code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,11 +3,4 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_training/main.dart';
 
-void main() {
-  testWidgets(
-    'Counter increments smoke test',
-    (tester) async {
-      await tester.pumpWidget(const MyApp());
-    },
-  );
-}
+void main() {}
