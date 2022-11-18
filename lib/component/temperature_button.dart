a2bec68b8e2e1c62c1b6ed12bcb5ab2aa843c8a8
ryutaroiseki <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-11-18 16:26:43 +0900

    add: add VoidCallback argument

--- lib/component/temperature_button.dart
+++ lib/component/temperature_button.dart
@@ -4,16 +4,18 @@ class TemperatureButton extends StatelessWidget {
   const TemperatureButton({
     super.key,
     required this.text,
+    required this.onPressed,
   });
 
   final String text;
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Center(
         child: TextButton(
-          onPressed: () {},
+          onPressed: onPressed,
           child: Text(text),
         ),
       ),
