8b36eb13b7b6b57edb94165b8647a5e45bbb871e
ryutaroiseki <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-11-23 16:31:07 +0900

    fix: move method to use mixin

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/mixin/home_mixin.dart';
+import 'package:flutter_training/mixin/mixin.dart';
+import 'package:flutter_training/screen/weather_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,7 +9,22 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> with HomeMixin {
+class _HomeScreenState extends State<HomeScreen> with Mixin {
+  @override
+  Future<void> loadScreen() async {
+    await Future<void>.delayed(const Duration(milliseconds: 500));
+    if (!mounted) {
+      return;
+    }
+    await Navigator.push(
+      context,
+      MaterialPageRoute<void>(
+        builder: (context) => const WeatherScreen(),
+      ),
+    );
+    await loadScreen();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(backgroundColor: Colors.green);
