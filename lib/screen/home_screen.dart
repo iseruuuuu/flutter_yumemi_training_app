2c4cdede7b2e122e4aefc2e77eda4b8321f02d61
ryutaroiseki <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-11-23 10:31:50 +0900

    add: import home_mixin and delete method

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/screen/weather_screen.dart';
+import 'package:flutter_training/mixin/home_mixin.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,28 +8,7 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  void initState() {
-    super.initState();
-
-    WidgetsBinding.instance.endOfFrame.then((_) => _loadScreen());
-  }
-
-  Future<void> _loadScreen() async {
-    await Future<void>.delayed(const Duration(milliseconds: 500));
-    if (!mounted) {
-      return;
-    }
-    await Navigator.push(
-      context,
-      MaterialPageRoute<void>(
-        builder: (context) => const WeatherScreen(),
-      ),
-    );
-    await _loadScreen();
-  }
-
+class _HomeScreenState extends State<HomeScreen> with HomeMixin {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(backgroundColor: Colors.green);
