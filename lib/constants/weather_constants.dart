f200da7785d38f60475dc7e33efc44758c5ec1a5
ryutaroiseki <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-11-29 11:02:48 +0900

    add: add constant to not change

---
+++ lib/constants/weather_constants.dart
@@ -0,0 +1,4 @@
+const String weatherCondition = 'weather_condition';
+const String maxTemperature = 'max_temperature';
+const String minTemperature = 'min_temperature';
+const String jsonFile = 'assets/json/weather.json';
