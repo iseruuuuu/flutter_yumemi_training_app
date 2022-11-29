b4f7315973ca7ee22e01c5a4b2d0dbbc373605a1
ryutaroiseki <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-11-29 11:01:42 +0900

    add: add temperature argument

--- lib/component/temperature_item.dart
+++ lib/component/temperature_item.dart
@@ -4,20 +4,23 @@ class TemperatureItem extends StatelessWidget {
   const TemperatureItem({
     super.key,
     required this.textColor,
+    required this.temperature,
   });
 
   final Color textColor;
+  final String temperature;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: MediaQuery.of(context).size.width / 4,
       child: Text(
-        '** ℃',
+        '$temperature ℃',
         style:
-            Theme.of(context).textTheme.labelLarge?.copyWith(color: textColor),
+        Theme.of(context).textTheme.labelLarge?.copyWith(color: textColor),
         textAlign: TextAlign.center,
       ),
     );
   }
 }
+
