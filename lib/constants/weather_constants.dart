04b50553db2557a133c266a0c59f2120be70366a
ryutaroiseki <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-12-05 15:12:56 +0900

    remove: remove json file to not use

--- lib/constants/weather_constants.dart
+++ lib/constants/weather_constants.dart
@@ -1,4 +1,8 @@
 const String weatherCondition = 'weather_condition';
 const String maxTemperature = 'max_temperature';
 const String minTemperature = 'min_temperature';
-const String jsonFile = 'assets/json/weather.json';
+const request = '''
+{
+  "area": "tokyo",
+  "date": "2020-04-01T12:00:00+09:00"
+}''';
