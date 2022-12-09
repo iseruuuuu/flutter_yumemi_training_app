188c79c52e7645f0744ff04108cfb1492e194b86
ryutaroiseki <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-12-09 15:23:23 +0900

    build: build build runner

--- lib/model/weather_request.g.dart
+++ lib/model/weather_request.g.dart
@@ -7,17 +7,9 @@ part of 'weather_request.dart';
 // **************************************************************************
 
 _$_WeatherRequest _$$_WeatherRequestFromJson(Map<String, dynamic> json) =>
-    $checkedCreate(
-      r'_$_WeatherRequest',
-      json,
-      ($checkedConvert) {
-        final val = _$_WeatherRequest(
-          area: $checkedConvert('area', (v) => v as String? ?? 'tokyo'),
-          date: $checkedConvert(
-              'date', (v) => v as String? ?? '2020-04-01T12:00:00+09:00'),
-        );
-        return val;
-      },
+    _$_WeatherRequest(
+      area: json['area'] as String? ?? 'tokyo',
+      date: json['date'] as String? ?? '2020-04-01T12:00:00+09:00',
     );
 
 Map<String, dynamic> _$$_WeatherRequestToJson(_$_WeatherRequest instance) =>
--- lib/model/weather_result.g.dart
+++ lib/model/weather_result.g.dart
@@ -7,24 +7,12 @@ part of 'weather_result.dart';
 // **************************************************************************
 
 _$_WeatherResult _$$_WeatherResultFromJson(Map<String, dynamic> json) =>
-    $checkedCreate(
-      r'_$_WeatherResult',
-      json,
-      ($checkedConvert) {
-        final val = _$_WeatherResult(
-          weatherCondition: $checkedConvert('weather_condition',
-              (v) => $enumDecode(_$WeatherConditionEnumMap, v)),
-          maxTemperature: $checkedConvert('max_temperature', (v) => v as int),
-          minTemperature: $checkedConvert('min_temperature', (v) => v as int),
-          date: $checkedConvert('date', (v) => DateTime.parse(v as String)),
-        );
-        return val;
-      },
-      fieldKeyMap: const {
-        'weatherCondition': 'weather_condition',
-        'maxTemperature': 'max_temperature',
-        'minTemperature': 'min_temperature'
-      },
+    _$_WeatherResult(
+      weatherCondition:
+          $enumDecode(_$WeatherConditionEnumMap, json['weather_condition']),
+      maxTemperature: json['max_temperature'] as int,
+      minTemperature: json['min_temperature'] as int,
+      date: DateTime.parse(json['date'] as String),
     );
 
 Map<String, dynamic> _$$_WeatherResultToJson(_$_WeatherResult instance) =>
