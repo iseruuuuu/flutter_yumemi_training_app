10bf5cf19825364fc5d26d596f34bdfa444e1655
ryutaroiseki <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-12-21 10:05:36 +0900

    add: create weather_forecast_panel and state

---
+++ lib/component/weather_forecast_panel.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_training/component/temperature_item.dart';
+import 'package:flutter_training/state/forecast_panel/weather_forecast_panel_state.dart';
+
+class WeatherForecastPanel extends ConsumerWidget {
+  const WeatherForecastPanel({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final deviceWidthSize = MediaQuery.of(context).size.width / 2;
+    final weatherForecastPanelState =
+        ref.watch(weatherForecastPanelStateProvider);
+    return Column(
+      children: [
+        Center(
+          child: SizedBox(
+            width: deviceWidthSize,
+            height: deviceWidthSize,
+            child: weatherForecastPanelState.weatherImage.isNotEmpty
+                ? SvgPicture.asset(
+                    'assets/images/${weatherForecastPanelState.weatherImage}.svg',
+                  )
+                : const Placeholder(),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: SizedBox(
+            width: deviceWidthSize,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TemperatureItem(
+                  textColor: Colors.blue,
+                  temperature: weatherForecastPanelState.minTemperature,
+                ),
+                TemperatureItem(
+                  textColor: Colors.red,
+                  temperature: weatherForecastPanelState.maxTemperature,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/state/forecast_panel/weather_forecast_panel_state.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'weather_forecast_panel_state.freezed.dart';
+
+@freezed
+class WeatherForecastPanelState with _$WeatherForecastPanelState {
+  const factory WeatherForecastPanelState({
+    @Default('') String weatherImage,
+    @Default('**') String maxTemperature,
+    @Default('**') String minTemperature,
+  }) = _WeatherForecastPanelState;
+}
+
+final weatherForecastPanelStateProvider =
+    StateProvider<WeatherForecastPanelState>(
+  (ref) {
+    return const WeatherForecastPanelState();
+  },
+);
