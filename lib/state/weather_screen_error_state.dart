dde1d873efa7094283a2630a1a87bf99324aa0d3
ryutaroiseki <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-12-21 10:30:15 +0900

    add: create error_state

---
+++ lib/state/weather_screen_error_state.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'weather_screen_error_state.freezed.dart';
+
+@freezed
+class WeatherScreenErrorState with _$WeatherScreenErrorState {
+  const factory WeatherScreenErrorState({
+    @Default('') String errorMessage,
+  }) = _WeatherScreenErrorState;
+}
+
+final weatherScreenErrorProvider = StateProvider<WeatherScreenErrorState>(
+  (ref) {
+    return const WeatherScreenErrorState();
+  },
+);
