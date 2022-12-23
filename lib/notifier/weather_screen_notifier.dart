9db427cfc501124a9fd284d6cc5dc0c09c00d5a1
ryutaroiseki <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-12-23 11:57:15 +0900

    fix: add load YumemiWeather and move the contents of the process in the repository.

--- lib/notifier/weather_screen_notifier.dart
+++ lib/notifier/weather_screen_notifier.dart
@@ -1,15 +1,19 @@
+import 'dart:convert';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_training/notifier/weather_repository_notifier.dart';
+import 'package:flutter_training/constants/weather_constants.dart';
+import 'package:flutter_training/gen/assets.gen.dart';
+import 'package:flutter_training/model/weather_result.dart';
 import 'package:flutter_training/state/weather_forecast_panel_state.dart';
-import 'package:flutter_training/state/weather_repository_ui_state.dart';
+import 'package:flutter_training/state/weather_request_state.dart';
 import 'package:flutter_training/state/weather_screen_error_state.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
 
 final weatherScreenNotifierProvider = Provider<WeatherScreenNotifier>(
   (ref) {
-    final weatherRepository = ref.watch(weatherRepositoryProvider);
+    final weatherRequestState = ref.watch(weatherRequestStateProvider);
     return WeatherScreenNotifier(
       ref: ref,
-      repositoryNotifier: weatherRepository,
+      weatherRequestState: weatherRequestState,
     );
   },
 );
@@ -17,36 +21,63 @@ final weatherScreenNotifierProvider = Provider<WeatherScreenNotifier>(
 class WeatherScreenNotifier extends StateNotifier<WeatherForecastPanelState> {
   WeatherScreenNotifier({
     required this.ref,
-    required this.repositoryNotifier,
+    required this.weatherRequestState,
   }) : super(const WeatherForecastPanelState());
 
   final Ref ref;
-  final WeatherRepositoryNotifier repositoryNotifier;
+  final WeatherRequestState weatherRequestState;
+  final _weather = YumemiWeather();
 
   Future<void> reloadWeather() async {
-    await repositoryNotifier.requestWeather();
-    final repositoryUiState = ref.read(weatherRepositoryUiStateProvider);
-    if (repositoryUiState.errorMessage == '') {
-      final weatherImage = repositoryUiState.weatherCondition;
-      final maxTemperature = repositoryUiState.maxTemperature.toString();
-      final minTemperature = repositoryUiState.minTemperature.toString();
+    try {
+      final encodeFromJson = jsonEncode(weatherRequestState.toJson());
+      final loadWeatherJson = _weather.fetchWeather(encodeFromJson);
+      final decodeFromJson =
+          jsonDecode(loadWeatherJson) as Map<String, dynamic>;
+      final result = WeatherResult.fromJson(decodeFromJson);
+      final weatherImage = result.weatherCondition;
+      final maxTemperature = result.maxTemperature.toString();
+      final minTemperature = result.minTemperature.toString();
       ref.read(weatherForecastPanelStateProvider.notifier).update(
         (state) {
           return WeatherForecastPanelState(
-            weatherImage: weatherImage,
+            weatherImageName: getImage(weatherImage),
             minTemperature: minTemperature,
             maxTemperature: maxTemperature,
           );
         },
       );
-    } else {
-      ref.read(weatherScreenErrorStateProvider.notifier).update(
-        (state) {
-          return WeatherScreenErrorState(
-            errorMessage: repositoryUiState.errorMessage,
-          );
-        },
-      );
+    } on YumemiWeatherError catch (error) {
+      ref.read(weatherScreenErrorStateProvider.notifier).update((state) {
+        return WeatherScreenErrorState(
+          errorMessage: convertYumemiWeatherError(error),
+        );
+      });
     }
+  }
+
+  String getImage(WeatherCondition weatherCondition) {
+    switch (weatherCondition) {
+      case WeatherCondition.sunny:
+        return Assets.images.sunny;
+      case WeatherCondition.cloudy:
+        return Assets.images.cloudy;
+      case WeatherCondition.rainy:
+        return Assets.images.rainy;
+    }
+    return '';
+  }
+
+  String convertYumemiWeatherError(YumemiWeatherError error) {
+    final String errorMessage;
+    switch (error) {
+      case YumemiWeatherError.unknown:
+        errorMessage = 'unknown';
+        break;
+      case YumemiWeatherError.invalidParameter:
+        errorMessage = 'invalidParameter';
+        break;
+    }
+    return errorMessage;
   }
 }
