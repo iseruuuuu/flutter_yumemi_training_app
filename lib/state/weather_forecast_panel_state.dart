b115ba88aaec5dcd5569d0fabc90e014cf3d01fd
ryutaroiseki <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-12-23 12:01:53 +0900

    remove: delete import

--- lib/state/weather_forecast_panel_state.dart
+++ lib/state/weather_forecast_panel_state.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_training/constants/weather_constants.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'weather_forecast_panel_state.freezed.dart';
