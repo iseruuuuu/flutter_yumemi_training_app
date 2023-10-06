35d97534d42c20b2b3861f5472070d5e71122c41
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2023-10-06 10:24:49 +0900

    add: providerScopeを追加しました。

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/screen/home_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_training/ui/home/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
