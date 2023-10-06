fe70c80804b5c1506ffd3ced12f6f898494e23e0
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2023-10-06 10:30:19 +0900

    add: constantsの移動・エラーメッセージをconstantsに追記しました

---
+++ lib/common/constants/error_message.dart
@@ -0,0 +1,5 @@
+class ErrorMessage {
+  static const invalidParameter = 'パラメータが間違っています。';
+  static const unknown = '予期せぬ不具合が発生しました。';
+  static const other = '例外が発生しました。';
+}
--- lib/constants/weather_constants.dart
+++
@@ -1,5 +0,0 @@
-enum WeatherCondition {
-  sunny,
-  cloudy,
-  rainy,
-}
