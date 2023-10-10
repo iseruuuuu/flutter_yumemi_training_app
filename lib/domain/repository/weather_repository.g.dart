bc71fb4b06a5a66a8a98a2be99675bd3b3b674e8
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2023-10-10 10:27:56 +0900

    fix: build.yamlを更新しました

---
+++ lib/domain/repository/weather_repository.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type, lines_longer_than_80_chars, unnecessary_null_checks, require_trailing_commas
+
+part of 'weather_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$weatherRepositoryHash() => r'7675a945c55579132dc2cf2e021a38e76d817b41';
+
+/// See also [weatherRepository].
+@ProviderFor(weatherRepository)
+final weatherRepositoryProvider =
+    AutoDisposeProvider<WeatherRepository>.internal(
+  weatherRepository,
+  name: r'weatherRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$weatherRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef WeatherRepositoryRef = AutoDisposeProviderRef<WeatherRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
