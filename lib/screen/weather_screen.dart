16105f88e9a152d7c8a9c29437ef7728c3a40b70
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2023-10-10 10:21:25 +0900

    add: screenをConsumerWidgetに変更しました

--- lib/screen/weather_screen.dart
+++ lib/screen/weather_screen.dart
@@ -1,138 +1,113 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_training/component/temperature_button.dart';
 import 'package:flutter_training/component/temperature_item.dart';
-import 'package:flutter_training/model/weather_request.dart';
-import 'package:flutter_training/model/weather_result.dart';
-import 'package:yumemi_weather/yumemi_weather.dart';
+import 'package:flutter_training/constants/weather_constants.dart';
+import 'package:flutter_training/screen/weather_screen_view_model.dart';
 
-class WeatherScreen extends StatefulWidget {
+class WeatherScreen extends ConsumerWidget {
   const WeatherScreen({super.key});
 
   @override
-  State<WeatherScreen> createState() => _WeatherScreenState();
-}
-
-class _WeatherScreenState extends State<WeatherScreen> {
-  final _weather = YumemiWeather();
-  String _weatherImage = '';
-  String _minTemperature = '**';
-  String _maxTemperature = '**';
-  final _weatherRequest = const WeatherRequest();
-
-  Future<void> _reloadWeather() async {
-    try {
-      final encodeFromJson = jsonEncode(_weatherRequest.toJson());
-      final loadWeatherJson = _weather.fetchWeather(encodeFromJson);
-      final decodeFromJson =
-          jsonDecode(loadWeatherJson) as Map<String, dynamic>;
-      final result = WeatherResult.fromJson(decodeFromJson);
-      _weatherImage = result.weatherCondition.name;
-      _maxTemperature = result.maxTemperature.toString();
-      _minTemperature = result.minTemperature.toString();
-      setState(() {});
-    } on YumemiWeatherError catch (error) {
-      final errorMessage = _convertYumemiWeatherError(error);
-      _openErrorDialog(errorMessage);
-    }
-  }
-
-  String _convertYumemiWeatherError(YumemiWeatherError error) {
-    final String errorMessage;
-    switch (error) {
-      case YumemiWeatherError.unknown:
-        errorMessage = 'unknown';
-        break;
-      case YumemiWeatherError.invalidParameter:
-        errorMessage = 'invalidParameter';
-        break;
-    }
-    return errorMessage;
-  }
-
-  void _openErrorDialog(String error) {
-    showDialog<void>(
-      context: context,
-      builder: (_) {
-        return AlertDialog(
-          title: const Text('Error'),
-          content: Text('error is $error'),
-          actions: [
-            TextButton(
-              onPressed: _onTapBack,
-              child: const Text('OK'),
-            ),
-          ],
-        );
-      },
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(weatherScreenViewModelProvider());
+    return Scaffold(
+      body: state.when(
+        list: (weather, max, min) {
+          return _WeatherBody(
+            ref: ref,
+            weatherCondition: weather,
+            maxTemperature: max,
+            minTemperature: min,
+          );
+        },
+        initial: () {
+          return _WeatherBody(
+            ref: ref,
+            weatherCondition: WeatherCondition.none,
+            maxTemperature: '**',
+            minTemperature: '**',
+          );
+        },
+      ),
     );
   }
+}
 
-  void _onTapBack() {
-    Navigator.of(context).pop();
-  }
+class _WeatherBody extends StatelessWidget {
+  const _WeatherBody({
+    required this.ref,
+    required this.weatherCondition,
+    required this.maxTemperature,
+    required this.minTemperature,
+  });
+
+  final WidgetRef ref;
+  final WeatherCondition weatherCondition;
+  final String maxTemperature;
+  final String minTemperature;
 
   @override
   Widget build(BuildContext context) {
     final deviceWidthSize = MediaQuery.of(context).size.width / 2;
-    return Scaffold(
-      body: Column(
-        children: [
-          const Spacer(flex: 2),
-          Center(
-            child: SizedBox(
-              width: deviceWidthSize,
-              height: deviceWidthSize,
-              child: _weatherImage.isNotEmpty
-                  ? SvgPicture.asset('assets/images/$_weatherImage.svg')
-                  : const Placeholder(),
-            ),
+    return Column(
+      children: [
+        const Spacer(flex: 2),
+        Center(
+          child: SizedBox(
+            width: deviceWidthSize,
+            height: deviceWidthSize,
+            child: weatherCondition != WeatherCondition.none
+                ? SvgPicture.asset('assets/images/${weatherCondition.name}.svg')
+                : const Placeholder(),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16),
-            child: SizedBox(
-              width: deviceWidthSize,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  TemperatureItem(
-                    textColor: Colors.blue,
-                    temperature: _minTemperature,
-                  ),
-                  TemperatureItem(
-                    textColor: Colors.red,
-                    temperature: _maxTemperature,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Column(
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: SizedBox(
+            width: deviceWidthSize,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                const SizedBox(height: 80),
-                SizedBox(
-                  width: deviceWidthSize,
-                  child: Row(
-                    children: [
-                      TemperatureButton(
-                        text: 'Close',
-                        onPressed: _onTapBack,
-                      ),
-                      TemperatureButton(
-                        text: 'Reload',
-                        onPressed: _reloadWeather,
-                      ),
-                    ],
-                  ),
+                TemperatureItem(
+                  textColor: Colors.blue,
+                  temperature: maxTemperature,
+                ),
+                TemperatureItem(
+                  textColor: Colors.red,
+                  temperature: minTemperature,
                 ),
               ],
             ),
           ),
-        ],
-      ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Column(
+            children: [
+              const SizedBox(height: 80),
+              SizedBox(
+                width: deviceWidthSize,
+                child: Row(
+                  children: [
+                    TemperatureButton(
+                      text: 'Close',
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    TemperatureButton(
+                      text: 'Reload',
+                      onPressed: () => ref
+                          .read(weatherScreenViewModelProvider().notifier)
+                          .reloadWeather(context),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
