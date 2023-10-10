61c1e81192df3f14c7ccbd9f5ca66c3e3ab5cc5a
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2023-10-10 10:21:56 +0900

    add:  WeatherScreenViewModelを作成しました

---
+++ lib/screen/weather_screen_view_model.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_training/domain/repository/weather_repository.dart';
+import 'package:flutter_training/model/weather_request.dart';
+import 'package:flutter_training/screen/weather_screen_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'weather_screen_view_model.g.dart';
+
+@riverpod
+class WeatherScreenViewModel extends _$WeatherScreenViewModel {
+  @override
+  WeatherScreenState build({
+    WeatherScreenState initState = const WeatherScreenState.initial(),
+  }) {
+    return initState;
+  }
+
+  void reloadWeather(BuildContext context) {
+    const weatherRequest = WeatherRequest();
+    ref.read(weatherRepositoryProvider).getWeather(weatherRequest).map(
+      success: (list) {
+        state = WeatherScreenState.list(
+          weatherCondition: list.value.weatherCondition,
+          maxTemperature: list.value.maxTemperature.toString(),
+          minTemperature: list.value.minTemperature.toString(),
+        );
+      },
+      failure: (error) {
+        //失敗したとしてもエラーDialogを出すだけで、stateの状態を更新しない。
+        _openErrorDialog(error.error, context);
+      },
+    );
+  }
+
+  void _openErrorDialog(String error, BuildContext context) {
+    showDialog<void>(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          title: const Text('Error'),
+          content: Text(error),
+          actions: [
+            TextButton(
+              onPressed: Navigator.of(context).pop,
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
---
+++ lib/screen/weather_screen_view_model.g.dart
@@ -0,0 +1,180 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type, lines_longer_than_80_chars, unnecessary_null_checks
+
+part of 'weather_screen_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$weatherScreenViewModelHash() =>
+    r'aa20981041f9b0e230bf02a5e09e7306f11a095e';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+abstract class _$WeatherScreenViewModel
+    extends BuildlessAutoDisposeNotifier<WeatherScreenState> {
+  late final WeatherScreenState initState;
+
+  WeatherScreenState build({
+    WeatherScreenState initState = const WeatherScreenState.initial(),
+  });
+}
+
+/// See also [WeatherScreenViewModel].
+@ProviderFor(WeatherScreenViewModel)
+const weatherScreenViewModelProvider = WeatherScreenViewModelFamily();
+
+/// See also [WeatherScreenViewModel].
+class WeatherScreenViewModelFamily extends Family<WeatherScreenState> {
+  /// See also [WeatherScreenViewModel].
+  const WeatherScreenViewModelFamily();
+
+  /// See also [WeatherScreenViewModel].
+  WeatherScreenViewModelProvider call({
+    WeatherScreenState initState = const WeatherScreenState.initial(),
+  }) {
+    return WeatherScreenViewModelProvider(
+      initState: initState,
+    );
+  }
+
+  @override
+  WeatherScreenViewModelProvider getProviderOverride(
+    covariant WeatherScreenViewModelProvider provider,
+  ) {
+    return call(
+      initState: provider.initState,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'weatherScreenViewModelProvider';
+}
+
+/// See also [WeatherScreenViewModel].
+class WeatherScreenViewModelProvider extends AutoDisposeNotifierProviderImpl<
+    WeatherScreenViewModel, WeatherScreenState> {
+  /// See also [WeatherScreenViewModel].
+  WeatherScreenViewModelProvider({
+    WeatherScreenState initState = const WeatherScreenState.initial(),
+  }) : this._internal(
+          () => WeatherScreenViewModel()..initState = initState,
+          from: weatherScreenViewModelProvider,
+          name: r'weatherScreenViewModelProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$weatherScreenViewModelHash,
+          dependencies: WeatherScreenViewModelFamily._dependencies,
+          allTransitiveDependencies:
+              WeatherScreenViewModelFamily._allTransitiveDependencies,
+          initState: initState,
+        );
+
+  WeatherScreenViewModelProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.initState,
+  }) : super.internal();
+
+  final WeatherScreenState initState;
+
+  @override
+  WeatherScreenState runNotifierBuild(
+    covariant WeatherScreenViewModel notifier,
+  ) {
+    return notifier.build(
+      initState: initState,
+    );
+  }
+
+  @override
+  Override overrideWith(WeatherScreenViewModel Function() create) {
+    return ProviderOverride(
+      origin: this,
+      override: WeatherScreenViewModelProvider._internal(
+        () => create()..initState = initState,
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        initState: initState,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeNotifierProviderElement<WeatherScreenViewModel, WeatherScreenState>
+      createElement() {
+    return _WeatherScreenViewModelProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is WeatherScreenViewModelProvider &&
+        other.initState == initState;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, initState.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+mixin WeatherScreenViewModelRef
+    on AutoDisposeNotifierProviderRef<WeatherScreenState> {
+  /// The parameter `initState` of this provider.
+  WeatherScreenState get initState;
+}
+
+class _WeatherScreenViewModelProviderElement
+    extends AutoDisposeNotifierProviderElement<WeatherScreenViewModel,
+        WeatherScreenState> with WeatherScreenViewModelRef {
+  _WeatherScreenViewModelProviderElement(super.provider);
+
+  @override
+  WeatherScreenState get initState =>
+      (origin as WeatherScreenViewModelProvider).initState;
+}
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
