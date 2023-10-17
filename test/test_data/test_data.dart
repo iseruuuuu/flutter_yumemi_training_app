9cdaf95628aaa144742b0a118545c23b478e7893
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2023-10-18 07:58:52 +0900

    fix: fix warning

--- test/test_data/test_data.dart
+++ test/test_data/test_data.dart
@@ -13,21 +13,21 @@ class TestData {
 
   static const invalidWeatherRequestData = {
     'area': 123,
-    'date': '2023-10-12T12:00:00+09:00'
+    'date': '2023-10-12T12:00:00+09:00',
   };
 
   static const weatherResultData = {
     'weather_condition': 'sunny',
     'max_temperature': 30,
     'min_temperature': 15,
-    'date': '2023-10-12T00:00:00.000'
+    'date': '2023-10-12T00:00:00.000',
   };
 
   static const invalidWeatherResultData = {
     'weather_condition': 123,
     'max_temperature': 30,
     'min_temperature': 15,
-    'date': '2023-10-12T00:00:00.000'
+    'date': '2023-10-12T00:00:00.000',
   };
 
   static const weatherJson = '''
