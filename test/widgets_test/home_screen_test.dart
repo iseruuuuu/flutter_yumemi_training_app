adb31ccc9e9a32535bec995b90647e9111940a40
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2023-10-23 15:51:50 +0900

    add: home_screenのテストコードを書いてみました。

---
+++ test/widgets_test/home_screen_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_training/main.dart';
+import 'package:flutter_training/view/home_screen.dart';
+import 'package:flutter_training/view/weather_screen.dart';
+
+import 'utils/utils.dart';
+
+void main() {
+  setUp(setDisplaySize);
+  tearDown(resetDisplaySize);
+
+  testWidgets(
+    'Automatic transition from HomeScreen to WeatherScreen after 500ms.',
+    (tester) async {
+      await tester.pumpWidget(const ProviderScope(child: MyApp()));
+      expect(find.byType(HomeScreen), findsOneWidget);
+      // 500m秒後の遅延実行する
+      await tester.pump(const Duration(milliseconds: 500));
+      await tester.pumpAndSettle();
+      expect(find.byType(HomeScreen), findsNothing);
+      expect(find.byType(WeatherScreen), findsOneWidget);
+    },
+  );
+}
